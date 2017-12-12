7c64e4f56d3f8e8c62ac30deda1adf9b46936c51
Osha Foster <oshafoster@hotmail.com>
2017-12-12 10:42:55 -0700

    ready to deploy

--- src/Footer.js
+++ src/Footer.js
@@ -5,8 +5,8 @@ function Footer(){
     return(
         <div className="footer">
             <div className="infoDiv">
-                <a href="https://www.linkedin.com/in/osha-foster-a295ab52/" target="_blank" rel="noopener noreferrer" className="resumeLink
-                "><span className="resume">Resume</span></a>
+                <a href="https://drive.google.com/file/d/1OrVF-NhYLsoSBY_64R0UlUqRzqyeNV5U/view?usp=sharing" target="_blank" rel="noopener noreferrer"
+                className="resumeLink"> <span className="resume">Resume</span></a>
                 <a href="https://www.linkedin.com/in/osha-foster-a295ab52/" target="_blank" rel="noopener noreferrer" className="icons"><span class="fa-stack fa-lg">
                         <i class="fa fa-circle fa-stack-2x"></i>
                     <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
--- src/Main.js
+++ src/Main.js
@@ -8,12 +8,22 @@ function Main() {
     return (
         <div className="parent">
             <div className="about">
-                <h3>About:</h3>
-            <p>Graduate of VSchool where I completed 500 hours of intensive web development training with a focus on: <br></br>
+                <h3>About: </h3>
+            <p>Graduate of VSchool where I completed 450 hours of intensive web development training with a focus on: <br></br>
 
         <span className="languages" >JavaScript, React.JS, Redux, Express, NodeJS, MongoDB, HTML5, CSS3.</span><br></br>
 
-   Learning to code is a continual process. I like that programming principles build on themselves, and there is always something to learn.
+        Learning to code is a continual process. I like that programming principles build on themselves, and there is always
+        something to learn. I worked independently as landscape gardener for years, and there were often large projects that
+        needed to be considered and pared down to manageable assignments. I am finding a similar experience in web development:
+        taking large tasks and breaking them into approachable pieces. <br></br>I enjoyed the experience of working with my
+        classmates (at times pair-programming) while building apps. I experienced the benefit of having two sets of eyes on the
+        code. I found it interesting, joining someone else on a project where I had to familiarize myself with their code. I
+        felt the process of collaboration often helped us move through problems efficiently. Working in teams, we started our
+        day with a 15 minute stand up and had SCRUM meetings with an advisor. Having a plan for the day and a way to measure our
+        progress was helpful. Since graduating, I am continuing my studies independently to strengthen my foundation. I look
+        forward to joining a company where I can continue to learn, grow and offer my skills.
+
                 </p>
             </div>
             <div className="titleProjects">Projects:</div>
